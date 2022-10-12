bfcfe1b4aec9e48b23f4a9e0bdcbc397b606c033
czarus99 <cezarprokoks@gmail.com>
2022-10-12 10:13:30 +0200

    prawiekoniexc

--- quiz.php
+++ quiz.php
@@ -17,9 +17,34 @@
         
         <div>
         <?php
-            $con = new mysqli("127.0.0.1", "root", "", "quiz");
-            $zap = $con->query("Select id, treść FROM pytania");
-            if()
+            $con = new mysqli("localhost", "root", "", "quiz");
+            $ile = $con->query("Select count(id) FROM pytania");
+            $max = $ile->fetch_array()[0];
+            $los = rand(1, $max);
+            
+            
+            $zap = $con->query("Select id, treść FROM pytania WHERE id=$los");
+            $wyp = $zap->fetch_all(MYSQLI_ASSOC);
+                for($i=0;$i<count($wyp); $i++){
+                    echo $wyp[$i]["treść"]."<br>";
+                    
+                }
+            ?>
+            
+            
+            
+            <?php
+            $odp = $con->query("SELECT `id`, `Treść` FROM `odpowiedzi` 
+                                JOIN pytania_has_odpowiedzi ON pytania_has_odpowiedzi.Odpowiedzi_id=odpowiedzi.id
+                                WHERE pytania_has_odpowiedzi.Pytania_id = $los");
+            
+            $wypodp = $odp->fetch_all(MYSQLI_ASSOC);
+                for($i=0; $i<count($wypodp); $i++){
+                    echo $wypodp[$i]["Treść"]."<br>";
+                }
+                
+            
+         
             
         ?>
         </div>
